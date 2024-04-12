ccec3be73d0dbc92083face6c3b3361cc46c36e8
Marius Pop <marstherobot@users.noreply.github.com>
2024-04-12 10:48:57 +0300

    Initial Commit

---
+++ ipc4/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Marius on 12.04.2024.
+//  Copyright © 2024. All rights reserved.
+//  
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
